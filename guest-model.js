d868d246471a225e92f1c533b913d2f934e1bca2
Tuan Nguyen <nvt2106@yahoo.com>
2016-08-29 00:41:14 +0700

    Add guest DOB is required

--- guest-model.js
+++ guest-model.js
@@ -13,6 +13,7 @@ GuestModel.validationRules = GuestModel.validationRules || {};
 rules = [];
 rules.push(new Validator.StringRule('first_name', 'First Name', true));
 rules.push(new Validator.StringRule('last_name', 'Last Name', true));
+rules.push(new Validator.DateRule('birth_date', 'Date Of Birth', true, '1900-01-01', Validator.Date.getTodayString()));
 rules.push(new Validator.StringRule(['address', 'address_line_1'], 'Address/Line1', true));
 rules.push(new Validator.NumberRule(['address', 'country_id'], 'Address/Country', true));
 GuestModel.validationRules.creatingObject = rules;
